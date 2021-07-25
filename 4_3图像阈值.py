9b01c1d39f6965462f84a84280e0d4f9d7103a62
Your Name <email@example.com>
2021-07-25 11:16:41 +0800

    Zhang commit

--- 4_3图像阈值.py
+++ 4_3图像阈值.py
@@ -1,3 +1,14 @@
+''' enum ThresholdTypes {
+    THRESH_BINARY     = 0,
+    THRESH_BINARY_INV = 1,
+    THRESH_TRUNC      = 2,
+    THRESH_TOZERO     = 3,
+    THRESH_TOZERO_INV = 4,
+    THRESH_MASK       = 7,
+    THRESH_OTSU       = 8,
+    THRESH_TRIANGLE   = 16      }'''
+
+    
 # 简单阈值
 import cv2 as cv
 import numpy as np
